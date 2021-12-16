2ecd16588c07c2a689f7fe39e13d8932d63fc267
kravz2702-byte <kravz2702@gmail.com>
2021-12-16 23:52:19 +0300

    Arrow funcs
    arrow funcs, object.freeze

--- useful.js
+++ useful.js
@@ -74,3 +74,68 @@ function checkScope(){
 }
 
 checkScope()
+
+//const
+
+function printMannyTimes(str){
+    "use strict";
+    const SENTENCE = str + "is cool!";
+    for (let i=0; i<str.length;i++){
+        console.log(SENTENCE);
+    }
+}
+
+//mutate an array declared wuith const
+
+const s = [7,5,2]
+
+function editPlace(){
+    "use strict";
+
+    s[0]=2;
+    s[1]=7;
+    s[2]=5;
+
+}
+
+editPlace();
+console.log(s);
+
+//Prevent object mutation
+
+function freezeObj(){
+    "use strict";
+    const MATH_CONSTANTS = {
+        PI:3.14
+    };
+
+    Object.freeze(MATH_CONSTANTS);
+
+    try{
+        MATH_CONSTANTS.PI=99;
+    } catch (ex){
+        console.log("ow nou");
+    }
+
+    return MATH_CONSTANTS.PI
+}
+
+
+//use arrow functions for concise anonymous functions
+const magic = () => new Date();
+
+//Arrow func with params
+
+const myConcat = (arr1, arr2)=> arr1.concat(arr2);
+
+console.log(myConcat([1,2],[3,4,5]));
+
+
+//Write Higher Order Arrow Functions
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+
+const SquareList = (arr) =>{
+    const squaredIntegers = arr.filter(num => Number.isInteger(num) && num>0).map(x=>x*x);
+    return squaredIntegers;
+}
