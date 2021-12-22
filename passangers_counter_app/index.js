ab60297235227dd1c6ec5a995ba2d1e24ac1e9c6
kravz2702-byte <kravz2702@gmail.com>
2021-12-22 23:14:45 +0300

    js
    js

---
+++ passangers_counter_app/index.js
@@ -0,0 +1,9 @@
+//document.getElementById("count-el").innerText = 11
+let countEl = document.getElementById("count-el")
+
+
+
+function increment(){
+    count = count+1
+    countEl.innerText=count
+}
