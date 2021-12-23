5ec62e80f3e8df51fdc0476f5abd8d1a571423bd
kravz2702-byte <kravz2702@gmail.com>
2021-12-23 21:48:19 +0300

    counter app
    complete passenger counter app

--- passangers_counter_app/index.js
+++ passangers_counter_app/index.js
@@ -1,9 +1,17 @@
-//document.getElementById("count-el").innerText = 11
+
+
 let countEl = document.getElementById("count-el")
-
-
+let saveEl = document.getElementById("save-el")
+let count = 0
 
 function increment(){
-    count = count+1
-    countEl.innerText=count
+    count += 1
+    countEl.textContent=count
+}
+
+function save(){
+    let saveCount = count + " - "
+    saveEl.textContent += saveCount
+    countEl.textContent = 0
+    count = 0
 }
