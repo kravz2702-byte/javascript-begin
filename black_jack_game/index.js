e1c7b285a57e0c2f9c7951a184c6758c7a672eda
kravz2702-byte <kravz2702@gmail.com>
2022-01-03 21:44:01 +0300

    added new feature
    added new feature

--- black_jack_game/index.js
+++ black_jack_game/index.js
@@ -1,14 +1,21 @@
-let firstCard = 10;
+let firstCard = 5;
 let secondCard = 11;
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = ""
 let messageEl = document.getElementById("message-el")
-
-
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame(){
+  renderGame()
+}
+
+function renderGame(){
+    cardsEl.textContent = "Cards: "+ cards[0] + " " + cards[1]
+    sumEl.textContent = "Sum: " + sum 
     if (sum <=20){
         message = "Do you want to draw a new card?"
       } else if (sum===21){
@@ -16,8 +23,14 @@ function startGame(){
         hasBlackJack = true
       } else{
         message = "You are out of the game"
-        isAlive = false
+        isAlive = false 
       }
     
     messageEl.textContent = message 
+}
+
+function newCard(){
+  let card = 3
+  sum += card
+  renderGame()
 }
